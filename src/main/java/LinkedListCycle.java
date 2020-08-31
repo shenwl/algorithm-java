9deae0da536e3900f872b62dcbc94d60b5a81b98
shenwl <shenweilun96@gmail.com>
2020-09-01 00:02:50 +0800

    leetcode 141 环形链表 - 快慢指针，空间O(1)

--- src/main/java/LinkedListCycle.java
+++ src/main/java/LinkedListCycle.java
@@ -5,23 +5,25 @@ import java.util.ArrayList;
 /**
  * leetcode 141
  * https://leetcode-cn.com/problems/linked-list-cycle/
- * 要求：O(1)空间复杂度的环检测
+ * 要求：O(1)空间复杂度的环检测 - 快慢指针
  */
 public class LinkedListCycle {
     public boolean hasCycle(ListNode head) {
-        if (head == null) return false;
+        if (head == null || head.next == null) return false;
 
-        ArrayList<ListNode> visited = new ArrayList<>();
+        ListNode slow = head;
+        ListNode quick = head.next;
 
-        ListNode cur = head;
+        while(quick != null) {
+            quick = quick.next;
 
-        while (cur != null) {
-            if (visited.contains(cur)) {
-                return true;
-            }
-            visited.add(cur);
-            cur = cur.next;
+            if(quick == slow) return true;
+            if(quick == null) return false;
+
+            quick = quick.next;
+            slow = slow.next;
         }
+
         return false;
     }
 }
