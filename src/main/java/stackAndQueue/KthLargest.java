dcc746292eca88c04ea5294f1a2c9fbfa4019ca6
shenwl <shenweilun96@gmail.com>
2020-09-08 22:51:01 +0800

    typo

--- src/main/java/stackAndQueue/KthLargest.java
+++ src/main/java/stackAndQueue/KthLargest.java
@@ -6,7 +6,7 @@ import java.util.PriorityQueue;
  * leetcode 703
  * https://leetcode-cn.com/problems/kth-largest-element-in-a-stream/
  * 方法1：记录保存k个最大值，每次进来一个书都进行排序，把里面最小的淘汰，时间复杂度: NKLogK
- * 方法2：优先队列，把最大/最小的元素放在最上面，这里采用的方法是维护一个MinHeap，size = k（k大元素在堆顶），时间复杂度log2K
+ * 方法2：优先队列，把最大/最小的元素放在最上面，这里采用的方法是维护一个MaxHeap，size = k（k大元素在堆顶），时间复杂度log2K
  */
 class KthLargest {
     final PriorityQueue<Integer> q;
